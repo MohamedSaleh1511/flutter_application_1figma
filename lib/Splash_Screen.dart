c5e504f9ca78b72b4ebbe1f6fcb79567fb401196
MohamedSaleh1511 <15112001mt@gmail.com>
2026-03-06 23:59:55 +0200

    feat(ui): add Travel header with Lobster font and SVG globe icon

--- lib/Splash_Screen.dart
+++ lib/Splash_Screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -12,8 +14,30 @@ class SplashScreen extends StatelessWidget {
             colors: [Color(0xff0172B2), Color(0xff001645)],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
-            
           ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Travel',
+                  style: GoogleFonts.lobster(
+                    fontSize: 35,
+                    color: Color(0xffFFFFFF),
+                  ),
+                ),
+                SizedBox(width: 10),
+                SvgPicture.asset(
+                  'assets/globe icon-1.svg',
+                  width: 35,
+                  height: 35,
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
